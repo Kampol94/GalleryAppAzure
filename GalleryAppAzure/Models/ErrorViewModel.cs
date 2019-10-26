bfb9009b45622699139fad75144c605ae9cc4474
kampolaczek <kampolaczek@gmail.com>
2019-10-26 18:02:13 +0200

    Dodaj pliki projektów.

---
+++ GalleryAppAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GalleryAppAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
